f2c56b0cf58bfaf3aabcdb2bd8ba6c28962a37ca
lanjinlong <ljl1941017510@163.com>
2019-07-30 17:01:31 +0800

    闲云首页

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <!-- 轮播图 -->
     <el-carousel :interval="5000" arrow="always">
       <el-carousel-item v-for="(item,index) in banners" :key="index">
         <div
@@ -8,6 +9,33 @@
         ></div>
       </el-carousel-item>
     </el-carousel>
+    <!-- 搜索框 -->
+    <!-- 搜索框 -->
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 -->
+        <el-row type="flex" class="search-tab">
+          <span
+            v-for="(item,index) in options"
+            :key="index"
+            :class="{active:current==index}"
+            @click="handleClick(index)"
+          >
+            <i>{{item.title}}</i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input
+            :placeholder="options[current].placeholder"
+            v-model="searchValue"
+            @keyup.enter="handleSearch"
+          />
+          <i class="el-icon-search" @click="handleSearch"></i>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,21 +43,48 @@
 export default {
   data() {
     return {
-      banners: []
+      banners: [],
+      options: [
+        { title: "攻略", placeholder: "搜索城市", pageUrl: "/post?city=" },
+        {
+          title: "酒店",
+          placeholder: "请输入城市搜索酒店",
+          pageUrl: "/hotel?city="
+        },
+        {
+          title: "机票",
+          placeholder: "请输入触发地",
+          pageUrl: "/air"
+        }
+      ],
+      searchValue: "", //搜索框的值
+      current: 0 //当前选中的值
     };
   },
   mounted() {
+    // $开头的属性是vue实例对象的约定用法
     this.$axios({
       url: "/scenics/banners"
     }).then(res => {
-      const { data } = res.data;
+      const { data } = res.data; //对象解构
       this.banners = data;
     });
+  },
+  methods: {
+    // 把index的值赋给cirrent
+    handleClick(index) {
+      // 点击机票
+      if (index === 2) {
+        this.$router.push("/air");
+      }
+      this.current = index;
+    },
+    handleSearch() {}
   }
 };
 </script>
 
-<style lang="less" scoped>
+<style scoped lang="less">
 .container {
   min-width: 1000px;
   margin: 0 auto;
@@ -43,6 +98,94 @@ export default {
     width: 100%;
     height: 100%;
   }
+
+  .banner-content {
+    z-index: 9;
+    width: 1000px;
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    margin-left: -500px;
+    border-top: 1px transparent solid;
+
+    .search-bar {
+      width: 552px;
+      margin: 0 auto;
+    }
+
+    .search-tab {
+      .active {
+        i {
+          color: #333;
+        }
+        &::after {
+          background: #eee;
+        }
+      }
+
+      span {
+        width: 82px;
+        height: 36px;
+        display: block;
+        position: relative;
+        margin-right: 8px;
+        cursor: pointer;
+
+        i {
+          position: absolute;
+          z-index: 2;
+          display: block;
+          width: 100%;
+          height: 100%;
+          line-height: 30px;
+          text-align: center;
+          color: #fff;
+        }
+
+        &:after {
+          position: absolute;
+          left: 0;
+          top: 0;
+          display: block;
+          content: "";
+          width: 100%;
+          height: 100%;
+          border: 1px rgba(255, 255, 255, 0.2) solid;
+          border-bottom: none;
+          transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+          transform-origin: bottom left;
+          background: rgba(0, 0, 0, 0.5);
+          border-radius: 1px 2px 0 0;
+          box-sizing: border-box;
+        }
+      }
+    }
+
+    .search-input {
+      width: 550px;
+      height: 46px;
+      background: #fff;
+      border-radius: 0 4px 4px 4px;
+      border: 1px rgba(255, 255, 255, 0.2) solid;
+      border-top: none;
+      box-sizing: unset;
+
+      input {
+        flex: 1;
+        height: 20px;
+        padding: 13px 15px;
+        outline: none;
+        border: 0;
+        font-size: 16px;
+      }
+
+      .el-icon-search {
+        cursor: pointer;
+        font-size: 22px;
+        padding: 0 10px;
+        font-weight: bold;
+      }
+    }
+  }
 }
 </style>
-
